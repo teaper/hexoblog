730d983a7db901fa3e54ff98d2167ea36fe1fda9
TEAPER <www@teaper.dev>
2020-01-07 12:52:31 +0800

    Site updated: 2020-01-07 12:52:31

--- js/search.js
+++ js/search.js
@@ -82,5 +82,5 @@ var searchFunc = function(path, search_id, content_id) {
         }
     })
 };
-var path = "https://www.teaper.dev/search.xml";
+var path = "http://"+window.location.host+"/search.xml";
 searchFunc(path, 'local-search-input', 'local-search-result');
